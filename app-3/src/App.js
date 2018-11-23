e0cce7951de7a91093ac42c095e5708f3ea72ce2
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-11-23 00:18:17 -0700

    assignment app-3 complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      items: ['computer', 'mouse', 'trackpad', 'keyboard', 'monitor'],
+      input: ''
+    }
+  }
+
+  handleInput(text){
+    this.setState({input: text})
+  }
+
   render() {
+    let mappedItems = this.state.items.filter( (element, index) => {
+      return element.includes(this.state.input)
+    }).map( (element,index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+    console.log(this.state.input)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={e => this.handleInput(e.target.value)} />
+        {mappedItems}
       </div>
     );
   }
