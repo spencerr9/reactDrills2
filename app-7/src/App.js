7583d631635e65787ec145a529fffea53444d4e2
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-12-05 20:14:42 -0700

    To do list is working properly

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      taskArray:[],
+      inputBox:'',
+    }
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleInput(text){
+    this.setState({
+      inputBox: text
+    })
+  }
+  
+  handleAdd(){
+    this.setState({
+      taskArray: [...this.state.taskArray, this.state.inputBox],
+      inputBox: ''
+    })
+  }
+
   render() {
+    let mappedTasks = this.state.taskArray.map((e, i) => <h2 key={i}>{e}</h2>)
+
+    console.log(this.state)
     return (
       <div className="App">
-      
+        <h1>My To Do List:</h1>
+        <input 
+          placeholder='Enter a task' 
+          onChange={(e) => this.handleInput(e.target.value)}
+          value={this.state.inputBox}
+        ></input>
+        <button onClick={(e) => this.handleAdd(e.target.value)} >Add</button>
+        {mappedTasks}
       </div>
     );
   }
